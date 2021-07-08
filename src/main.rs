5100683fe5f4fc5e98a6a0828bc67a4c822701f2
Rahix <rahix@rahix.de>
2021-07-08 19:23:09 +0200

    Initial version

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    loop {
+        arduino_hal::delay_ms(1000);
+    }
+}
