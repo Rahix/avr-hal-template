2add49d688e9822f2a3f7c4d115960a6f3e2e4dc
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2023-11-06 13:53:58 +0100

    feat: add sparkfun-promini-3v

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ fn main() -> ! {
         "Arduino Uno",
         "Nano168",
         "Adafruit Trinket Pro",
+        "SparkFun ProMini 3v",
         "SparkFun ProMini 5v"
       -%}
     let mut led = pins.d13.into_output();
